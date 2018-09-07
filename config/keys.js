221c47963659c504ecaa2f956316629965409e9e
Jake McC <jakemccarthy@gmail.com>
2018-09-07 03:36:12 +0100

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://jake:Falcons1@ds235022.mlab.com:35022/storybooks-dev',
+    googleClientID:'619575192214-6hc9unmvsp8utagm30p5fgl5n5njo08v.apps.googleusercontent.com',
+    googleClientSecret: '0-1UBzI1Cdd0cFtxkuftWJod'
+}
